dce9e20d13ed4fcc587f3beb18cc0ae955308cf9
Umar <123856634+Umulla-4006@users.noreply.github.com>
2023-06-05 08:12:57 +0530

    Add files via upload

---
+++ online-shopping/src/main/java/com/onlineshopping/service/ProductServiceImpl.java
@@ -0,0 +1,32 @@
+package com.onlineshopping.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import com.onlineshopping.dao.ProductDao;
+import com.onlineshopping.model.Product;
+import com.onlineshopping.utility.StorageService;
+
+
+@Service
+public class ProductServiceImpl implements ProductService {
+	
+	@Autowired 
+	private ProductDao productDao;
+	
+	@Autowired
+	private StorageService storageService;
+
+	@Override
+	public void addProduct(Product product, MultipartFile productImmage) {
+		
+		String productImageName = storageService.store(productImmage);
+		
+		product.setImageName(productImageName);
+		
+		System.out.println("checking");
+		
+		this.productDao.save(product);
+	}
+
+}
